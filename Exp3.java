cca23a8ec61d4ebe9bf7059a2c55c75591676608
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-05-20 21:36:32 +0530

    add Experiment number 3

---
+++ Exp3.java
@@ -0,0 +1,13 @@
+class Exp3{
+public static void main(String s[]){
+	String str1 = "tara";
+	String str2 = "chand";
+	
+	if(str1.equals(str2)){
+	System.out.println("String is equal");
+}
+else{
+	System.out.println("string is not equal");
+}
+}
+}
