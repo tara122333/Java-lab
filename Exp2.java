b1c60effa64f766e7830a8b002adc763fb7675e6
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-05-20 21:35:34 +0530

    add Experiment number 2

---
+++ Exp2.java
@@ -0,0 +1,24 @@
+class Exp2{
+	public static void main(String arr[]){
+		System.out.println("Hello Tara");
+		int num=12;
+		short s=1;
+		byte b=1;
+		float f=12.35f;
+		char c='c';
+		boolean boo=true;
+		long ln = 156;
+		double dd=456.233;
+		String str = new String();
+		str = "Tara Chand";
+		System.out.println(num);
+		System.out.println(s);
+		System.out.println(b);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(boo);
+		System.out.println(ln);
+		System.out.println(dd);
+		System.out.println(str);
+	}
+}
