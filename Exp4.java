979636cf45aacd00be83412df51edd75c4cb1c35
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-05-20 21:37:28 +0530

    add Experiment number 4

---
+++ Exp4.java
@@ -0,0 +1,9 @@
+public class Exp4{
+public static void main(String arr[]){
+	// int a[]=new int[10];
+	int a[] = {1,2,50,77,32,2,123,06,07,8,9,9999};
+	for(int x:a){
+	System.out.println(x + " ");
+}
+}
+}
