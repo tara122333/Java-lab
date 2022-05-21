0c6afeda922b7c5eeb90d02e978b2dea7e7404ec
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-05-21 13:23:17 +0530

    add For each loop

--- Exp5.java
+++ Exp5.java
@@ -4,33 +4,36 @@ class Exp5{
 		arr[0] = new int [3];
 		arr[1] = new int [3];
 		arr[2] = new int [3];
-	arr[3] = new int [3];
-
+		arr[3] = new int [3];
 		arr[0][0] = 4;
-arr[0][1] = 5;
-arr[0][2] = 6;
-arr[1][0] = 7;
-arr[1][1] = 8;
-arr[1][2] = 7;
-arr[2][0] = 1;
-arr[2][1] = 13;
-arr[2][2] = 8;
-arr[3][0] = 8;
-arr[3][1] = 1;
-arr[3][2] = 55;
+		arr[0][1] = 5;
+		arr[0][2] = 6;
+		arr[1][0] = 7;
+		arr[1][1] = 8;
+		arr[1][2] = 7;
+		arr[2][0] = 1;
+		arr[2][1] = 13;
+		arr[2][2] = 8;
+		arr[3][0] = 8;
+		arr[3][1] = 1;
+		arr[3][2] = 55;
 	
-
 	int min = arr[0][0];
-	for(int i = 0;i<4;i++){
-	for(int j=0;j<3;j++){
-	if(arr[i][j]<min){
-		min = arr[i][j];
+// 	for(int i = 0;i<arr.length;i++){
+// 	for(int j=0;j<arr[i].length;j++){
+// 	if(arr[i][j]<min){
+// 		min = arr[i][j];
+// 	}
+// }
+// }
+	// int i = 0;
+	for(int x[]:arr){
+		for(int y:x){
+			if(y<min){
+				min = y;
+			}
+		}
 	}
-}
-}
 	System.out.println(min);
-	
-		
-
 	}
 }
