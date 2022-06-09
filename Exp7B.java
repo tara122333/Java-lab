2f2821c36f9c6a174f90b8ad78d7373bc38f8e44
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-06-09 23:53:49 +0530

    not a part of Exp

--- Exp7B.java
+++ Exp7B.java
@@ -1,5 +1,3 @@
-import javax.sound.midi.SysexMessage;
-
 class Box{
     private double width;
     private double height;
---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String arg[]) {
+        final double var = 56;
+        // var = 566; // Error bcoz var is constant 
+        System.out.println(var);
+    }
+}
